66a8da5e95e5461895252a37d01742b8924b6e75
jacopo1010 <jaorru03@gmail.com>
2025-06-11 21:59:00 +0200

    Modello di Dominio

--- Siw-books/src/main/java/it/uniroma3/siw/controller/LibrelandiaController.java
+++ Siw-books/src/main/java/it/uniroma3/siw/controller/LibrelandiaController.java
@@ -1,13 +1,10 @@
 package it.uniroma3.siw.controller;
 
-import it.uniroma3.siw.model.Utente;
 import it.uniroma3.siw.service.AutoreService;
 import it.uniroma3.siw.service.LibroService;
 import it.uniroma3.siw.service.RecensioneService;
-import it.uniroma3.siw.sessionData.SessionData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -19,7 +16,7 @@ public class LibrelandiaController {
 
 
     @GetMapping("/admin/indexAdmin")
-    public String adminHompage(){
+    public String adminHomepage(){
         return "/admin/indexAdmin.html";
     }
 
