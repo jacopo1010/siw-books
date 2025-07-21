50eff3a8c6f6cf4a08b663e54208764c4bd70a57
jacopo1010 <166029437+jacopo1010@users.noreply.github.com>
2025-07-21 20:54:12 +0200

    sviluppato un po di fe

--- Siw-books/src/main/java/it/uniroma3/siw/controller/GlobalController.java
+++ Siw-books/src/main/java/it/uniroma3/siw/controller/GlobalController.java
@@ -5,8 +5,11 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import static it.uniroma3.siw.model.Credenziali.ADMIN_ROLE;
 
 @ControllerAdvice
 public class GlobalController {
@@ -19,6 +22,26 @@ public class GlobalController {
             u = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         }
         return u;
+    }
+
+    @ModelAttribute
+    public void addGlobalAttributes(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        boolean isAnonymous = auth == null || auth instanceof AnonymousAuthenticationToken;
+        model.addAttribute("isAnonymous", isAnonymous);
+
+        // Se l'utente non è anonimo puoi aggiungere altre informazioni
+        if(!isAnonymous) {
+            Object principal = auth.getPrincipal();
+            if(principal instanceof UserDetails) {
+                boolean isAdmin = auth.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals(ADMIN_ROLE));
+                model.addAttribute("isAdmin", isAdmin);
+            }
+            else {
+                model.addAttribute("isAdmin", false);
+            }
+        }
     }
 
 }
