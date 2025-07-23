d9df460f13e13aa2bea3dad63952668c45f3c071
jacopo1010 <166029437+jacopo1010@users.noreply.github.com>
2025-07-23 14:55:44 +0200

    fine css login e registrazione + fix

--- Siw-books/src/main/java/it/uniroma3/siw/controller/LibrelandiaController.java
+++ Siw-books/src/main/java/it/uniroma3/siw/controller/LibrelandiaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class LibrelandiaController {
 
     @Autowired private LibroService libroService;
